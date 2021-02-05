468722e41f5c6d8b30bcbcced59e821e56c1cf47
IB <ilya_bond@mail.ru>
2021-02-05 12:32:06 +0300

    bd

--- db/model.js
+++ db/model.js
@@ -1,7 +1,35 @@
-const { connect, model, Schema, pluralize } = require('mongoose')
+const { connect, model, Schema, pluralize } = require('mongoose');
 
+pluralize(null);
+
+const userSchema = new Schema({
+  name: String,
+  login: String,
+  password: String,
+  // score: Number,
+})
+
+// const themeSchema = new Schema({
+//   theme: String,
+//   questions: [{
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'question'
+//   }]
+// })
+
+const questionSchema = new Schema({
+  question: String,
+  // price: Number,
+  answer1: String,
+  answer2: String,
+  answer3: String,
+  answer4: String,
+  rightAnswer: String,
+})
 
 
 module.exports = {
   connect,
+  User: model('users', userSchema),
+  Question: model('question', questionSchema),
 }
